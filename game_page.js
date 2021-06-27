cdd0c0c90fa24e07db00279c4b75ab980cad7320
Dawoon-Jung02 <80097639+Dawoon-Jung02@users.noreply.github.com>
2021-06-27 15:39:56 +0900

    Create game_page.js

---
+++ game_page.js
@@ -0,0 +1,13 @@
+// Name       : Dawoon Jung
+// Assignment : Make a Game
+// Course     : CS099
+// Spring 2021
+
+const first_screen = 1;
+const producer_screen = 2;
+const play_screen = 3;
+const easy_mode_screen = 10;
+const hard_mode_screen = 11;
+
+const p1_goal_screen = 20;
+const p2_goal_screen = 21;
